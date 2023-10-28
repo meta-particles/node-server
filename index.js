a6af8ef388d824cf857d4fffbcb80ddd7fbb2246
metaparticles <metaparticlesin@gmail.com>
2023-10-28 11:14:27 +0530

    new files

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./dbCon.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 const port = 8000;
