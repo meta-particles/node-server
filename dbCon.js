96cd2f1e31bb8d989a39c963d847ccc5d6ffe72e
metaparticles <metaparticlesin@gmail.com>
2023-10-28 11:18:54 +0530

    new cha

--- dbCon.js
+++ dbCon.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: process.env.DB_HOST, 
-  user: process.env.DB_USERNAME, 
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "root@123",
+  database: process.env.DB_DBNAME || "kalvikudam",
 });
